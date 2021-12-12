3528da36ba642243f77410913625bd3e74d5e51d
Rashel Rojas <rdr2139@columbia.edu>
2021-12-12 03:18:22 -0500

    working on app.py

--- app.py
+++ app.py
@@ -7,12 +7,28 @@ app = Flask(__name__)
 url = 'https://127.0.0.1:5000/'
 
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
 @app.route('/', methods=['GET'])
 def index():
     #response = requests.get(url)
     #return jsonify(response.json())
     
-    return jsonify(request.get_json())
+    return jsonify({'tasks': tasks})
+    #return jsonify(request.get_json())
     '''name = request.json['name']
 
     create_row_data = {'name': str(name)}
