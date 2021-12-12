dc0accbe65b4f4eec21d158eb06597bb5f5a8dc1
Rashel Rojas <rdr2139@columbia.edu>
2021-12-12 13:56:36 -0500

    got post to work

--- testing.py
+++ testing.py
@@ -1,16 +1,43 @@
+'''import requests
+
+response = requests.post("https://rashelserver.herokuapp.com", data={"name": "rashely"})
+print(response.status_code)
+print(response.text)
+'''
+
+
 from flask import Flask, jsonify, json, request, redirect, url_for, flash
 import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    api_url = 'https://lioneats.herokuapp.com/'
-    create_row_data = {'name': "rashel"}
-    r = requests.post(url=api_url, json=create_row_data)
-    print("the issue: ", r.status_code, r.reason, r.text)
-    return(r.text)
+
+    url = 'https://rashelserver.herokuapp.com'
+    data = {"name": "rashel"}
+    r = requests.post(url=url, json=data)
+    print("the response: ", r.status_code, r.reason, r.text)
+
+    return r.text
+    # this works too 1:52pm
+    # return json.dumps(response.json())
+
+
+
+
+@app.route('/ignore', methods=['GET'])
+def ignore():
+    # this works! when / in app.py is 'GET' and return jsonify({'tasks': tasks})
+    url = 'https://rashelserver.herokuapp.com'
+
+    response = requests.get(url)
+
+    print(str(response))
+    print('')
+    print(json.dumps(response.json(), indent=4))
+    return jsonify(response.text)
 
 
 if __name__ == '__main__':
